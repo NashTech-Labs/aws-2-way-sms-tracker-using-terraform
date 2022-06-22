1d7329e196c949741ad1929d9f9d22a8fcc197fc
sakshigawande12 <sakshi.gawande@knoldus.com>
2022-06-23 01:08:33 +0530

    outputs.tf and variables.tf added for pinpoint module

---
+++ modules/pinpoint/outputs.tf
@@ -0,0 +1,12 @@
+output "pinpoint_application_id" {
+ value = aws_pinpoint_app.test-notifications.application_id
+}
+
+output "pinpoint_application_name" {
+ value = aws_pinpoint_app.test-notifications.name
+}
+
+output "pinpoint_application_arn" {
+ value = aws_pinpoint_app.test-notifications.arn
+}
+
